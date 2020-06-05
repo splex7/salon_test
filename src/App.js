04313a0f8daa276b58a5685102fa5671bbc55d97
Hyungju Moon <splex7@gmail.com>
2020-06-05 21:38:18 +0900

    Posts not found

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import * as React from "react";
 import { Admin, Resource, EditGuesser } from 'react-admin'
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './users';
-import { PostList, PostEdit, PostShow} from './Posts';
+import { PostList, PostEdit, PostShow} from './posts';
 
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
